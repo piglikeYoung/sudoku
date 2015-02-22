13f1efbf07e99f6b59049dafe5b9808b02e0c4a6
piglikeyoung <piglikeyoung@piglikeungdeMBP.lan>
2015-02-22 14:02:46 +0800

    Initial Commit

---
+++ 九宫格2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  九宫格2
+//
+//  Created by piglikeyoung on 15/2/22.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 九宫格2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  九宫格2
+//
+//  Created by piglikeyoung on 15/2/22.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
